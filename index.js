cdbe339a89da1830eadc5b22c269e5a43481fb4b
Toph <topher@bythecode.agency>
2018-05-22 14:30:45 -0400

    suemedha most-of-the-way-done review

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ $(".image-gallery").each(function() {
     var ratio = calculateAspectRatio($(this).children("img"));
     $(this).css("flex-grow", ratio);
 });
+$(".slider-for").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    adaptiveHeight: true,
+    asNavFor: ".slider-nav"
+});
+$(".slider-nav").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
+});
+$("#imageModal").on("shown.bs.modal", function(e) {
+    $(".slider-for, .slider-nav").resize();
+    $(".slider-for, .slider-nav")[0].slick.setPosition();
+});
