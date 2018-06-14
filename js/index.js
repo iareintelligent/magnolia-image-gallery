8f955f8a94215da068ef1cae52b5338fcb7a5b02
Toph <topher@bythecode.agency>
2018-06-14 19:16:16 -0400

    updated for touch?

--- js/index.js
+++ js/index.js
@@ -54,3 +54,28 @@ $(".image-gallery img").on("click", function() {
     $("#imgDropdown").collapse("show");
     $(".slider").slick("slickGoTo", $(this).data("index"), true);
 });
+
+function is_touch_device() {
+    var prefixes = " -webkit- -moz- -o- -ms- ".split(" ");
+    var mq = function(query) {
+        return window.matchMedia(query).matches;
+    };
+
+    if (
+        "ontouchstart" in window ||
+        (window.DocumentTouch && document instanceof DocumentTouch)
+    ) {
+        return true;
+    }
+
+    // include the 'heartz' as a way to have a non matching MQ to help terminate the join
+    // https://git.io/vznFH
+    var query = ["(", prefixes.join("touch-enabled),("), "heartz", ")"].join(
+        ""
+    );
+    return mq(query);
+}
+
+if (is_touch_device()) {
+    $("#click-zone").hide();
+}
